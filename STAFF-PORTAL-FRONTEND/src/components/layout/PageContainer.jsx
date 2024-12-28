7d2b73ec011cafdbf236e94a137fa1dd43ac807d
soni ponnan <soniponnan@gmail.com>
2024-12-28 07:16:40 +0530

    did Mobile Optmization

--- STAFF-PORTAL-FRONTEND/src/components/layout/PageContainer.jsx
+++ STAFF-PORTAL-FRONTEND/src/components/layout/PageContainer.jsx
@@ -1,6 +1,6 @@
 export default function PageContainer({ children }) {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 w-full">
+    <div className="p-1.5 sm:p-4 lg:p-8 w-full">
       <div className="max-w-7xl mx-auto">{children}</div>
     </div>
   );
